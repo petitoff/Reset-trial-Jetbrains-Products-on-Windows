4edf95b079ae3fdb64d74ba6cb793d595fd52a74
Nikitin Arseniy <129211480+Mr-Darkson@users.noreply.github.com>
2024-12-11 13:18:33 +0300

    Update reset-trial-jetbrains.sh
    Change language to English

--- reset-trial-jetbrains.sh
+++ reset-trial-jetbrains.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-# Определяем имена IDE
+# Define IDE names
 IDE_NAMES=("WebStorm" "IntelliJ" "CLion" "Rider" "GoLand" "PhpStorm" "Resharper" "PyCharm")
 
-# Удаляем папку eval с лицензионным ключом и файлом options.xml
+# Remove eval folder with license key and options.xml file
 for IDE in "${IDE_NAMES[@]}"; do
     for dir in "${HOME}/.${IDE}*" ; do
         if [[ -d "$dir/config/eval" ]]; then
             rm -rf "$dir/config/eval"
-            echo "Удалена папка: $dir/config/eval"
+            echo "Removed folder: $dir/config/eval"
         fi
         if [[ -f "$dir/config/options/other.xml" ]]; then
             rm -f "$dir/config/options/other.xml"
-            echo "Удален файл: $dir/config/options/other.xml"
+            echo "Removed file: $dir/config/options/other.xml"
         fi
     done
 done
 
-# Удаляем папку JetBrains в .config
+# Remove JetBrains folder in .config
 JETBRAINS_DIR="${HOME}/.config/JetBrains"
 if [[ -d "$JETBRAINS_DIR" ]]; then
     rm -rf "$JETBRAINS_DIR"
-    echo "Удалена папка: $JETBRAINS_DIR"
+    echo "Removed folder: $JETBRAINS_DIR"
 fi
 
-# Удаляем ключ реестра JavaSoft (в Linux это не требуется, но можно удалить конфигурации Java, если есть)
-# Например, если у вас есть файл конфигурации Java, вы можете удалить его так:
+# Remove JavaSoft registry key (in Linux this is not required, but you can remove Java configurations if any)
+# For example, if you have a Java configuration file, you can remove it like this:
 JAVA_SOFT_DIR="${HOME}/.java"
 if [[ -d "$JAVA_SOFT_DIR" ]]; then
     rm -rf "$JAVA_SOFT_DIR"
-    echo "Удалена папка: $JAVA_SOFT_DIR"
+    echo "Removed folder: $JAVA_SOFT_DIR"
 fi
 
-echo "Скрипт завершен."
+echo "Script completed."
